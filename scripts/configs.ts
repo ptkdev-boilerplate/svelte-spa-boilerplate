2dc3fed2756e10b5b9c23017a911bc68d171ee76
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2021-03-18 23:55:00 +0100

    [Release] v1.0.0

---
+++ scripts/configs.ts
@@ -0,0 +1,10 @@
+/**
+ * Check configs.js
+ * =====================
+ * Check if configs/config.js exist, if don't exist rename .tpl
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ * @license: MIT License
+ *
+ */
